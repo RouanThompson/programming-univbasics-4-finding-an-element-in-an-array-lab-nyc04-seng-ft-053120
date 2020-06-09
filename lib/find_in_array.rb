b37034bd97f51fa237891c2105ef3bdc90529d7c
Rouan Thompson <rouan.thompson@gmail.com>
2020-06-09 04:14:58 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,11 @@
 def find_element_index(array, value_to_find)
-  array.include?(value_to_find)
+  i = 0
+
+  if array.include?(value_to_find)
+    while array[i] != value_to_find
+      i += 1
+    end
+    return i
+  else
+    nil
 end
